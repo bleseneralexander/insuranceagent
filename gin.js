81fc619601b45d19d024e2ad59783c9f247f4fda
bleseneralexander <113368267+bleseneralexander@users.noreply.github.com>
2023-09-07 16:44:40 -0500

    Create gin.js

---
+++ gin.js
@@ -0,0 +1,6 @@
+document.domain == "www.cabelas.com";
+innerhtml().document;
+print("Enjoy a life insurance policy today, email stlouisblues95@yahoo.com");
+<button onClick="alexanderblesner@yahoo.com">
+  <input type="submit" placeholder="email alexanderblesener@yahoo.com for a life insurance policy today></input>
+  </button>
